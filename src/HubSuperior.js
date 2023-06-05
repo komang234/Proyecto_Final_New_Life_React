3c0e58cf9054edd5e194460db873461d679f56c1
ULISES ANIBAL BAAMONDE <45687573@ort>
2023-06-05 16:02:21 -0300

    arreglando el proyecto

--- src/HubSuperior.js
+++ src/HubSuperior.js
@@ -1,19 +1,50 @@
 import './index.css';
 import './App.css';
-import logo from './logo.svg';
-import perfil from './perfil.png'
+import 'bootstrap/dist/css/bootstrap.css';
+import profileImage from './perfil.png';
+import gestorImage from './gestor-image.jpg';
+import logo from './logo.png';
+import edificio from './building.svg'
+
 
 function HubSuperior(){
 return (
-<header>
-    <nav className="navbar navbar-light bg-light">
-    <img src={logo} className='izquierda' width="30" height="30" alt="" />
-    <img src={perfil} className='derecha izquierda' width="30" height="30" alt="" />
-    <button type="button" className='btn btn-outline-primary derecha'>Primary</button>
-    <button type="button" className='btn btn-outline-primary derecha'>Primary</button>
+<div className="main-screen">
+        <nav className='navbar bg-body-tertiary'>
+        <div className='container-fluid'>
+        <img className='navbar-brand logo' src={logo} alt="New Life" width="30" height="24"/>
+        <ul class="navbar-nav">
+        <button className="login-button nav-item">Iniciar sesión</button>
+        <button className="register-button nav-item">Registrarse</button>
+        </ul>
+        <div>
+        <img src={profileImage} alt="Foto de perfil" className="profile-image" />
+        </div>
+        </div>
+        </nav>
+        
+        
+        
+        <div className="gestor">
+        <img src={gestorImage} alt="Imagen de gestor" className="gestor-image" />
+        </div>
+        <h1 className='center-name'> <u>Sergio Pomaroli</u></h1>
+        <div className="center-buttons">
+        <button className="reviews-button">Reseñas</button>
+        <button className="faq-button">Preguntas frecuentes</button>
+        <button className="citizenships-button"> Ciudadanias</button>
+        <img className="faq-button" src={edificio} alt="Ciudadanias"/>
+        </div>
 
-</nav>
-</header>
+        <div className="description">
+        <span className='border border-dark'>
+          Gestor con 30 años de experiencia en el ambiente laboral. Especialista en las ciudadanías portuguesa, italiana,
+          polaca, y española. Poseo contactos en las embajadas y consulados de estos países. Aviso todos los detalles del
+          trámite.
+        </span>
+      </div>
+      
+    </div>
 );
 
 
