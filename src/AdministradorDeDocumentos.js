3684472c8467bed83afb5633e1d2f1b01380849a
ULISES ANIBAL BAAMONDE <45687573@ort>
2023-08-07 12:06:14 -0300

    Mejorando la muestra de tramites por cliente

--- src/AdministradorDeDocumentos.js
+++ src/AdministradorDeDocumentos.js
@@ -7,100 +7,102 @@ import profileImage from './perfil.png';
 import logo from './logo.png';
 //import { responsivePropType } from 'react-bootstrap/esm/createUtilityClasses';
 
-function AdministradorDeDocumentos(){
+function ListaTramites({cliente}) {
+  { console.log("111 lista de tramites") }
+  let lista = JSON.parse(cliente.ListaTramites)
+  { console.log(lista) }
+  return (
+    lista.map((tramite) => {
+      return (
+        <div className='col-3'>
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">{tramite.Nombre}</h5>
+              <p className="card-text">{tramite.Descripción}</p>
+              <button className="btn btn-primary">Go somewhere</button>
+            </div>
+          </div>
+        </div>
+      )
+    })
+  )
+}
 
-  const [Cliente, setCliente] =  useState([])
+function ListaDeClientes({clientes}) {
+  return (
+    clientes.map((c) => {
+      return (
+        <div>
+          <h4>{c.nombre}</h4>
+          <hr className='separadorTramites'></hr>
+          <div className='container'>
+            <div className='row'>
+              <ListaTramites cliente={c} />
+            </div>
+          </div>
+        </div>
+      )
+    })
+  )
+}
+
+
+function AdministradorDeDocumentos() {
+
+  const [clientes, setClientes] = useState([])
   const [isLoading, setIsLoading] = useState(true);
 
 
 
   useEffect(() => {
-      
-      fetch("http://localhost:5000/gestores/clientes/1")
 
-        .then((response) => response.json())
+    fetch("http://localhost:5000/gestores/clientes/1")
+      .then((response) => response.json())
+      .then((clienteJson) => {
+        console.log("cliente", clienteJson)
+        setClientes(clienteJson)
+        setIsLoading(false)
+      });
+  }, []);
 
-        .then((clienteJson) => {
+  
 
-          console.log("cliente",clienteJson)
-
-          setCliente(clienteJson)
-
-          setIsLoading(false)
-
-        });
-
-
-    }, []);
-
-   function listaTramites(Lista){
-    Lista.ListaTramites.map( (tramite) => {
-        return(
-
-       <div className='col-3'>
-          <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">{tramite.Nombre}</h5>
-            <p className="card-text">{tramite.Descripción}</p>
-            <a href="index.html" class="card-link">Ver Mas Informacion</a>
+  return (
+    <div className="Contenedor-Mayor">
+      <nav className='navbar bg-body-tertiary border-header-top'>
+        <div className='container-fluid Padre'>
+          <img className='navbar-brand logo' src={logo} alt="New Life" width="30" height="24" />
+          <ul className='arreglarbotones'>
+            <li className="nav-item border izquierda">
+              <a className='nav-link active' aria-current="page" href="sdfs.html">Iniciar sesión</a>
+            </li>
+            <li className="nav-item border izquierda">
+              <a className='nav-link active' aria-current="page" href="sdfsdf.html" >Registrarse</a>
+            </li>
+          </ul>
+          <div>
+            <img src={profileImage} alt="Foto de perfil" className="profile-image" />
           </div>
         </div>
-        </div>
-        )
-        })
-   }
-      
-    
-    const listaDeClientes = Cliente.map( (cliente) => {
-     return(
-     <div>
-     <h4>{cliente.Nombre}</h4>
-     <hr className='separadorTramites'></hr>
-      <div className='container'> 
-      <div className='row'> 
-        {listaTramites(cliente)}
-      </div>
-      </div>
-     </div>
-     )
-    })
+      </nav>
 
-    return(
-        <div className="Contenedor-Mayor">
-        <nav className='navbar bg-body-tertiary border-header-top'>
-        <div className='container-fluid Padre'>
-        <img className='navbar-brand logo' src={logo} alt="New Life" width="30" height="24"/>
-        <ul className='arreglarbotones'>
-              <li className="nav-item border izquierda">
-                <a className='nav-link active' aria-current="page" href="sdfs.html">Iniciar sesión</a> 
-              </li>
-              <li className="nav-item border izquierda">
-                     <a className='nav-link active' aria-current="page" href="sdfsdf.html" >Registrarse</a>
-                 </li>
-        </ul>
-        <div>       
-        <img src={profileImage} alt="Foto de perfil" className="profile-image" />
-        </div>
-        </div>
-        </nav>
-        
-        <br className='separador'></br>
-        <div className="tramites"> 
+      <br className='separador'></br>
+      <div className="tramites">
         <h2> Lista de Tramites </h2>
         <hr></hr>
         <button type="button" className='btn btn-light derecha border botonesDeAgregacion'> + Agregar Tramite </button>
-        <button type="button" className='btn btn-light derecha border botonesDeAgregacion'> + Agregar Cliente </button> <br/><br/>
-        </div>
+        <button type="button" className='btn btn-light derecha border botonesDeAgregacion'> + Agregar Cliente </button> <br /><br />
+      </div>
 
-         
-            { 
-             
-             (!isLoading && listaDeClientes)
-            } 
-          </div>
-        
 
-    );
+      {
+
+        (!isLoading && <ListaDeClientes clientes={clientes} />)
+      }
+    </div>
+
+
+  );
 }
 
 export default AdministradorDeDocumentos;
--- src/EditorDeTramites.js
+++ src/EditorDeTramites.js
@@ -1,11 +1,34 @@
 import './index.css';
 import './App.css';
 import React from 'react';
+import { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import profileImage from './perfil.png';
 import logo from './logo.png';
 
 function EditorDeTramites(){
+
+  const [Etiqueta, setEtiqueta] =  useState([])
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+      
+    fetch("http://localhost:5000/etiquetas")
+
+      .then((response) => response.json())
+
+      .then((etiquetaJson) => {
+
+        console.log("etiqueta",etiquetaJson)
+
+        setEtiqueta(etiquetaJson)
+
+        setIsLoading(false)
+
+      });
+
+
+  }, []);
 
 return (
 <div className="Contenedor-Mayor">
@@ -39,8 +62,36 @@ return (
         <div className='Padre mb-3'>
           <form> <br/>
             <input className='centrarElementos formularioTexto form-text' type='text' id='peticion' name='peticion'/> <br/><br/>
+            
+            <div class="btn-group">
+                <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                 Right-aligned menu example
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end">
+             <li><button class="dropdown-item" type="button">Action</button></li>
+              <li><button class="dropdown-item" type="button">Another action</button></li>
+             <li><button class="dropdown-item" type="button">Something else here</button></li>
+           </ul>
+          </div>
+           {
+            /* <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Estados de tramite ya existentes
+            </button> 
+            <ul className="dropdown-menu">
+            <li><button class="dropdown-item" type="button">Action</button></li>
+              <li><button class="dropdown-item" type="button">Another action</button></li>
+             <li><button class="dropdown-item" type="button">Something else here</button></li>
+ 
+            
+            </ul>
+            </div>*/
+           }
 
-            Agregar estado del tramite: <input className='' type='text'name='Estado Tramites'/> <br/><br/>
+            <br/><br/>
+
+
+            Agregar nuevo estado del tramite: <input className='' type='text'name='Estado Tramites'/> <br/><br/>
             <a href="AdministradorDeDocumentos.js"><input className='centrarElementos btn btn-light border border-dark' type='submit' value='Lista de Tramites' /></a> <br/><br/>
             <input className='centrarElementos btn btn-light border border-dark' type='submit' value='Guardar cambios' /> 
           </form>
