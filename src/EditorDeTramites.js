ebfcf2b680956c0de2e800c14e40ac9acf0d5628
SimonChama23 <111370571+SimonChama23@users.noreply.github.com>
2023-10-09 10:28:25 -0300

    Update EditorDeTramites.js

--- src/EditorDeTramites.js
+++ src/EditorDeTramites.js
@@ -1,73 +1,55 @@
 import './index.css';
 import './App.css';
-import React from 'react';
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import profileImage from './perfil.png';
 import logo from './logo.png';
 
 function EditorDeTramites() {
-
-  const { contextState, setContextState } = useContextState();
-  const [Etiqueta, setEtiqueta] = useState([])
+  const [etiqueta, setEtiqueta] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [tramiteData, setTramiteData] = useState({
+    nombre: "",
+    descripcion: "",
+    estadoTramites: "",
+    archivos: [],
+  });
 
   useEffect(() => {
-
     fetch("http://localhost:5000/etiquetas")
-
       .then((response) => response.json())
-
       .then((etiquetaJson) => {
-
-        console.log("etiqueta", etiquetaJson)
-
-        setEtiqueta(etiquetaJson)
-
-        setIsLoading(false)
-
+        console.log("etiqueta", etiquetaJson);
+        setEtiqueta(etiquetaJson);
+        setIsLoading(false);
       });
-
-
   }, []);
+
   function uploadFile() {
-
     const fileInput = document.getElementById('fileInput');
-
-
     const file = fileInput.files[0];
 
     if (file) {
-      const formData = new FormData();
-      formData.append('file', file);
+      const nuevosArchivos = [...tramiteData.archivos, file];
+      setTramiteData({ ...tramiteData, archivos: nuevosArchivos });
 
-
-      const xhr = new XMLHttpRequest();
-
-
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-
-            document.getElementById('status').innerHTML = 'File uploaded successfully.';
-          } else {
-
-            document.getElementById('status').innerHTML = 'Error: ' + xhr.statusText;
-          }
-        }
-      };
-
-
-      xhr.open('POST', '/your-upload-endpoint', true);
-
-      xhr.send(formData);
+      document.getElementById('status').innerHTML = 'File uploaded successfully.';
     } else {
-
       document.getElementById('status').innerHTML = 'Please select a file to upload.';
-
     }
   }
-  return contextState.login ? (
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setTramiteData({ ...tramiteData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Datos del trámite:", tramiteData);
+  };
+
+  return (
     <div className="Contenedor-Mayor">
       <nav className='navbar bg-body-tertiary border-header-top'>
         <div className='container-fluid Padre'>
@@ -75,85 +57,80 @@ function EditorDeTramites() {
           <ul className='arreglarbotones'>
           </ul>
           <div>
-
             <img src={profileImage} alt="Foto de perfil" className="profile-image" />
           </div>
         </div>
       </nav>
 
-
-
-      <h3 className='center-name'>Editor de Tramite</h3>
+      <h3 className='center-name'>Editor de Trámite</h3>
       <div className="center-buttons">
-        <form> <br />
-          Nombre del tramite: <input className='' type='text' name='Nombre' /> <br /><br />
-          Descripción del tramite: <input className='' type='text' name='Descripción' /> <br /><br />
-
-        </form>
-
-
-      </div>
-
-      <br />
-      <h3 className='center-name'><u>Documentos:</u></h3>
-      <div className='Padre mb-3'>
-        <form> <br />
-
-
-          Agregar nuevo estado del tramite: <input className='' type='text' name='Estado Tramites' /> <br /><br />
+        <form onSubmit={handleSubmit}>
+          <br />
+          Nombre del trámite:
+          <input
+            className=''
+            type='text'
+            name='nombre'
+            value={tramiteData.nombre}
+            onChange={handleInputChange}
+          />
+          <br /><br />
+          Descripción del trámite:
+          <input
+            className=''
+            type='text'
+            name='descripcion'
+            value={tramiteData.descripcion}
+            onChange={handleInputChange}
+          />
+          <br /><br />
+          Agregar nuevo estado del trámite:
+          <input
+            className=''
+            type='text'
+            name='estadoTramites'
+            value={tramiteData.estadoTramites}
+            onChange={handleInputChange}
+          />
+          <br /><br />
           <input type="file" id="fileInput" />
-          <button onclick="uploadFile()">Subir Archivo</button>
+          <button type="button" onClick={uploadFile}>Subir Archivo</button>
           <div id="status"></div>
 
-          {
-
-            (!isLoading)
-          }
-
-          <a href="AdministradorDeDocumentos.js"><input className='centrarElementos btn btn-light border border-dark' type='submit' value='Lista de Tramites' /></a> <br /><br />
+          {!isLoading && (
+            <a href="AdministradorDeDocumentos.js"><input className='centrarElementos btn btn-light border border-dark' type='submit' value='Lista de Trámites' /></a>
+          )}
+          <br /><br />
           <input className='centrarElementos btn btn-light border border-dark' type='submit' value='Guardar cambios' />
-          <br className='separador'></br>
+          <br className='separador' />
           <div className="tramites">
-
-            <hr></hr>
-
+            <hr />
           </div>
 
-
-
-
-          {
-            /* <div className="dropdown">
+          <div className="dropdown">
             <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Estados de tramite ya existentes
-            </button> 
+              Estados de trámite ya existentes
+            </button>
             <ul className="dropdown-menu">
-            <li><button class="dropdown-item" type="button">Action</button></li>
-              <li><button class="dropdown-item" type="button">Another action</button></li>
-             <li><button class="dropdown-item" type="button">Something else here</button></li>
- 
-            
+              <li><button className="dropdown-item" type="button">Action</button></li>
+              <li><button className="dropdown-item" type="button">Another action</button></li>
+              <li><button className="dropdown-item" type="button">Something else here</button></li>
             </ul>
-            </div>*/
-          }
-
+          </div>
           <br /><br />
-
-
-
+          <div>
+            <h3>Archivos Subidos:</h3>
+            <ul>
+              {tramiteData.archivos.map((file, index) => (
+                <li key={index}>{file.name}</li>
+              ))}
+            </ul>
+          </div>
         </form>
       </div>
       <br />
     </div>
-  ) : (
-    <div className="Contenedor-Mayor">
-      <div className='contenedorAlerta'>
-        <p className='alerta'>  Atención! No se le permite usar la pagina debido a que no ha iniciado sesión. Vaya a la página principal para hacerlo.</p>
-      </div>
-    </div>
-  )
-
-
+  );
 }
 
 export default EditorDeTramites;
